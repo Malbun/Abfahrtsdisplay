2ef965c19a6bf5955f82acf08883b9ef93d0131c
Malbun <malbun08@mailfence.com>
2026-09-02 14:51:27 +0200

    Save work on server

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 import os
 
+"""
 if os.environ.get('DISPLAY', '') == '':
     os.environ['DISPLAY'] = ':0'
 
 window = tk.Tk()
 window.title("Abfahrtsdisplay")
-window.attributes("-zoomed", True);
+window.attributes("-zoomed", True)
 
 window.mainloop()
+"""
